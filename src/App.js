4e88286f6a22a5cb605e296eda5f0014bac46675
Chan <ieejo716@naver.com>
2021-03-17 19:43:44 +0900

    git ignore

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import MyComponent from "./MyComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <MyComponent/>
+        );
+    }
+}
+export default App;

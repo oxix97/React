bd0737c41d4f4a15257bffb9543bd543b8b6bf34
Chan <ieejo716@naver.com>
2021-03-31 18:38:21 +0900

    setState, useState

--- react/react_basic/props/src/event-basic/eventHandling.js
+++ react/react_basic/props/src/event-basic/eventHandling.js
@@ -1,26 +1,44 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
 class ButtonEffect extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            number: 0
+            number: 0,
+            message : ''
+        }
+        this.useState ={
+            number: this.state.number
         }
     }
 
     render() {
         return (
             <div>
-                <button onClick={() => {
+                <h1>이벤트 연습</h1>
+                <p>버튼 입력 여부 : {this.state.number}</p>
+                <input
+                    type="text"
+                    name={"message"}
+                    placeholder={"아무거나 입력해보세요."}
+                    value={this.state.message}
+                    onChange={
+                        (event => {
+                            this.setState({
+                                message : event.target.value
+                            })
+                        })
+                    }
+                />
+
+                <button onClick={()=>{
+                    alert(this.state.message);
                     this.setState({
-                        number: this.state.number + 1
-                    })
-                }
-                }>더하기
-                </button>
+                        number : this.useState.number + 1
+                    });
+                }}>확인</button>
             </div>
-        )
+        );
     }
 }
 

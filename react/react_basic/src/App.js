80aad6f1ec6dd20cacbc0db2b6ef475555469395
Chan <ieejo716@naver.com>
2021-04-06 22:37:20 +0900

    react

--- react/react_basic/src/App.js
+++ react/react_basic/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {Switch,Route} from 'react-router-dom'
 
 function App(){
   return (
       <div>
+          <Switch>
+              <Route path="/">
 
+              </Route>
+          </Switch>
       </div>
   )
 }

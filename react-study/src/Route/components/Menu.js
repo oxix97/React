2d558d71606c1cc22c2dfaedd9c13e221ff913a3
Chan <ieejo716@naver.com>
2021-04-12 22:59:44 +0900

    NavLink apply

--- react-study/src/Route/components/Menu.js
+++ react-study/src/Route/components/Menu.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Menu = () => {
+    const activeStyle = {
+        color : 'green',
+        fontSize : '2rem'
+    };
     return(
         <div>
             <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to={'/About'}>About</Link></li>
-                <li><Link to={'/about/react'}>React</Link></li>
+                <li><NavLink exact to='/' activeStyle={activeStyle}>Home</NavLink></li>
+                <li><NavLink exact to={'/About'} activeStyle={activeStyle}>About</NavLink></li>
+                <li><NavLink to={'/about/react'} activeStyle={activeStyle}>React</NavLink></li>
             </ul>
         </div>
     )

fb21dd932ab6866e3dea251d5dcd3b7264ec9e77
Chan <ieejo716@naver.com>
2021-04-21 22:13:27 +0900

    mock 서버에서 axios 받기

--- react/react-market/src/market/index.js
+++ react/react-market/src/market/index.js
@@ -1,12 +1,28 @@
 import './index.css';
+import axios from "axios";
+import React, {useEffect, useState} from 'react';
 
 function MainPage() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://a25c0af4-74d9-4935-b479-c512eabf252a.mock.pstmn.io/products')
+            .then(function (result) {
+                const products = result.data.products;
+                setProducts(products);
+            })
+            .catch(function (error) {
+                console.error("error : ", error);
+            });
+    }, []);
+
     return (
         <div>
+            {/*<Server/>*/}
             <div id="header">
                 <div id="header-area">
                     <img src="images/icons/logo.png" alt="icons"/>
-
                 </div>
             </div>
 
@@ -17,26 +33,32 @@ function MainPage() {
                 </div>
                 <h1>판매되는 상품들</h1>
                 <div id="product-list">
-                    <div className="product-card">
-                        <img src="images/products/basketball1.jpeg" alt="notebook1" className="product-img"/>
-                        <div className="product-contents">
-                        <span className="product-name">
-                            농구공
-                        </span>
-                            <span className="product-price">
-                            30,000원
-                        </span>
-                        </div>
-                        <div className={"product-seller"}>
-                            <img src="images/icons/avatar.png" alt="" className="product-avater"/>
-                            <span>스티브 잡스</span>
-                        </div>
-                    </div>
+                    {products.map(function (products, index) {
+                        return (
+                            <div className="product-card">
+                                <img alt="ball" className="product-img"
+                                     src={products.imageUrl}/>
+                                <div className="product-contents">
+                                    <span className="product-name">
+                                        {products.name}
+                                    </span>
+                                    <span className="product-price">
+                                        {products.price}원
+                                        </span>
+                                </div>
+                                <div className={"product-seller"}>
+                                    <img src="images/icons/avatar.png" alt="" className="product-avater"/>
+                                    <span>{products.seller}</span>
+                                </div>
+                            </div>
+                        );
+                    })
+                    }
                 </div>
             </div>
 
             <div id="footer">
-
+                아무 내용 없음
             </div>
 
         </div>

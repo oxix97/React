9228c7f9c8d58ea379d93a301b80e06a71deba11
Chan <ieejo716@naver.com>
2021-05-18 15:50:27 +0900

    prop-types

---
+++ clone-movie/src/Props/Potato.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class ComponentBasic extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('hello')
+    }
+
+    state = {
+        count: 0,
+    }
+
+    add = () => {
+        this.setState(number => ({
+                count: number.count + 1
+            })
+        )
+    }
+
+    minus = () => {
+        this.setState(number => ({
+                count: number.count - 1
+            })
+        )
+    }
+
+    componentDidMount() {
+        console.log('component rendered');
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('I just updated')
+    }
+
+    componentWillUnmount() {
+        console.log('cruel now')
+    }
+
+    render() {
+        console.log('render');
+        return (
+            <div>
+                <h1>The number is : {this.state.count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        )
+    }
+}
+
+export default ComponentBasic;

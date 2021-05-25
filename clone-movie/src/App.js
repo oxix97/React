a936b8b311cffd68ec8cf7450e09228e5ad2fa43
Chan <ieejo716@naver.com>
2021-05-26 01:13:30 +0900

    직업 추가

--- clone-movie/src/App.js
+++ clone-movie/src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import {Switch,Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import {
     Begin, ReportUser,
     RoleDistribution,
     Vote, VoteResult,
 } from "./Mafia/index";
-import {GlobalStyle,MoiveStyle} from './AppStyle'
-import Index from "./Movies";
+
 function App() {
-    return(
-        // <Switch>
-        //     <Route exact path={'/'} component={Begin}/>
-        //     <Route exact path={'/meetingRoom'} component={RoleDistribution}/>
-        //     <Route exact path={'/vote'} component={Vote}/>
-        //     <Route exact path={'/voteResult'} component={VoteResult}/>
-        //     <Route exact path={'/reporterVote'} component={ReportUser}/>
-        // </Switch>
-        <>
-            <GlobalStyle/>
-            <MoiveStyle>
-                <Index/>
-            </MoiveStyle>
-        </>
+    return (
+        <Switch>
+            <Route exact path={'/'} component={Begin}/>
+            <Route exact path={'/meetingRoom'} component={RoleDistribution}/>
+            <Route exact path={'/vote'} component={Vote}/>
+            <Route exact path={'/voteResult'} component={VoteResult}/>
+            <Route exact path={'/reporterVote'} component={ReportUser}/>
+        </Switch>
     )
 }
 

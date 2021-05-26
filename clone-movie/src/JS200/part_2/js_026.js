ea38b7c4946e234d6f5700867f14f027e7692405
Chan <ieejo716@naver.com>
2021-05-26 10:47:42 +0900

    Js 200

---
+++ clone-movie/src/JS200/part_2/js_026.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Js_026(){
+    //연산자에 의한 자료형 변환
+    // console.log('5'+1);
+    // console.log('5'-1);
+    // console.log('5'*2);
+    // console.log('5'/2);
+    //덧셈은 문자열, 숫자열을 반환하지만 , 뺄셈, 곱셈, 나눗셈은 항상 숫자형을 반환한다.
+
+    let str = 5+'1';
+    console.log(str);
+    console.log(typeof str);
+    let num = +str;
+    console.log(num);
+    console.log(typeof num)
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Js_026;
---
+++ clone-movie/src/JS200/part_2/js_028.js
@@ -0,0 +1,28 @@
+function Js_028(){
+
+    let family = {
+        'addresss' : 'Seoul',
+        members : {},
+        addFamily : function(age,name,role){
+            this.members[role] = {
+                age : age,
+                name: name,
+            };
+        },
+        getHeadcount : function(){
+            return Object.keys(this.members).length;
+        }
+    };
+
+    family.addFamily(30,'chan','aunt');
+    family.addFamily(3,'yu','niece');
+    family.addFamily(10,'daa','dog');
+    console.log(family.getHeadcount());
+    console.log(family)
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default Js_028

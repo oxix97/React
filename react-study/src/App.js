8d0d26dc4324b6426db7adc60bfcb06c6554d907
Chan <ieejo716@naver.com>
2021-05-27 17:56:41 +0900

    commit

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
-// import Name from "./JSX/names";
-// import Hello from "./JSX/Hello";
-// JSX 영역
+import ChildComponent from "./Component/ChildComponent";
 
-import {Route} from 'react-router-dom';
-import {
-    Home,
-    About,
-    Posts,
-    Pad
-} from './Route/pages';
-import Menu from "./Route/components/Menu";
-import SignIn from "./Modal/SignIn";
-import Button from "./Modal/Button";
-import StateValue from "./State/stateValue";
-import InputValue from "./State/InputValue";
-import TodoList from "./TodoList/Todo";
-function App() {
-    return (
-        <>
-            {/*<Menu/>*/}
-            {/*<Route exact path='/' component={Home}/>*/}
-            {/*<Route path='/About/:name?' component={About}/>*/}
-            {/*<Route path='/Posts' component={Posts}/>*/}
-            {/*<Button/>*/}
-            {/*<StateValue/>*/}
-            {/*<Route exact path = '/pad' component={Pad}/>*/}
-            {/*<InputValue />*/}
-            <TodoList/>
-        </>
-    );
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <ChildComponent
+                    boolValue={true}
+                    numValue={1}
+                    arrayValue={[1,2,3]}
+                    objValue={{name: 'title', age : 30}}
+                    nodeValue={<h1>node</h1>}
+                    funcValue={() => {console.log('message');}}
+                />
+            </div>
+        )
+    }
 }
 
 export default App;

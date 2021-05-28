50bca371fb9aa1cc98cf711f4a12a5b53b6e2f06
Chan <ieejo716@naver.com>
2021-05-28 16:19:50 +0900

    LifeCycle

--- do-it-react/src/Component/NewCounter.js
+++ do-it-react/src/Component/NewCounter.js
@@ -6,11 +6,25 @@ class NewCounter extends React.Component{
         this.state = {};
         this.increaseCount = this.increaseCount.bind(this);
     }
+    static getDerivedStateFromProps(props,state){
+        const {count} = props;
+        return{
+            count,
+            NewCount : count === state.count ? state.NewCount : count,
+        }
+    }
+    increaseCount(){
+        this.setState(({NewCount})=> ({
+            NewCount : NewCount + 1
+        }))
+    }
     render(){
         return(
             <div>
-                
+                <span>현재 카운트 : {this.state.NewCount}</span>
+                <button onClick={this.increaseCount}>카운트 증가</button>
             </div>
         )
     }
 }
+export default NewCounter;

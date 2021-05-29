c2d44010dbd161e9b26adf6f59af65ccc220f626
Chan <ieejo716@naver.com>
2021-05-29 23:23:27 +0900

    경찰 이벤트 적용

--- clone-movie/src/Mafia/nightFrame/Night.js
+++ clone-movie/src/Mafia/nightFrame/Night.js
@@ -1,20 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import ReportUser from "./ReportUser";
+import DoctorUser from "./DoctorUser";
+import PoliceUser from "./PoliceUser";
 
+const Display = styled.div`
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+`
+const Frame = styled.div`
+    border : 5px solid green;
+    margin : 3%;
+    padding : 50px;
+`
 
 function Night() {
     return (
-        <div>
-            <div className={'Reporter'}>
+        <Display>
+            <Frame>
+                <h3>Reporter Frame</h3>
                 <ReportUser/>
-            </div>
-            <div className={'police'}>
-
-            </div>
-            <div>
-
-            </div>
-        </div>
+            </Frame>
+            <Frame>
+                <h3>Doctor Frame</h3>
+                <DoctorUser/>
+            </Frame>
+            <Frame>
+                <h3>Police Frame</h3>
+                <PoliceUser/>
+            </Frame>
+            <Frame>
+                <h3>Gangster</h3>
+            </Frame>
+            <Frame>
+                <h3>Mafia</h3>
+            </Frame>
+        </Display>
 
     )
 };
---
+++ clone-movie/src/Mafia/nightFrame/PoliceUser.js
@@ -0,0 +1,41 @@
+import {useState} from "react";
+import {Users} from "../meetingFrame/Users";
+
+function PoliceUser() {
+    const [index, setIndex] = useState(0);
+    const [vote, setVote] = useState(Users[index].nickname)
+    const [isClick, setIsClick] = useState(false);
+    const [policeVote, setPoliceVote] = useState(false);
+
+    const onChange = e => {
+        setVote(e.target.value);
+    };
+
+    const onClick = e => {
+        const playerIndex = e.target.value;
+        setIndex(playerIndex);
+        setPoliceVote(Users[playerIndex].role === 'Mafia')
+        setIsClick(true);
+    };
+
+    return (
+        <div>
+            <select onChange={onChange}>
+                {Users.map((user, index) => (
+                    <option key={index} value={index}>{user.nickname}</option>
+                ))}
+            </select>
+            <button onClick={onClick} disabled={isClick} value={vote}>수사하기</button>
+            {
+                isClick ? (
+                    policeVote ? (
+                        <h3>마피아가 맞습니다.</h3>
+                    ) : <h3>마피아가 아닙니다.</h3>
+                ) : null
+            }
+        </div>
+    )
+
+}
+
+export default PoliceUser

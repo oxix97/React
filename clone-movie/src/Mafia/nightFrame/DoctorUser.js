c8ca7591b1c9d0523349b0fb1c69852e3c683ee4
Chan <ieejo716@naver.com>
2021-05-30 19:05:41 +0900

    doctor

---
+++ clone-movie/src/Mafia/nightFrame/DoctorUser.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import {Users} from "../meetingFrame/Users";
+function DoctorUser(){
+    const [index,setIndex] = useState(0);
+    const [vote, setVote] = useState(Users[index].nickname)
+    const [isClick, setIsClick] = useState(false);
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default DoctorUser;
